a705b35902256383f30492dc543c304a9de463fa
m3dus23 <dimamedus@gmail.com>
2023-07-20 17:28:27 +0300

    v1

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import App from '../App'
 import LoginPage from '../pages/Login/LoginPage'
 
 const Router = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route element={<App />} path='/' />
 				<Route element={<LoginPage />} path='/login' />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
 
